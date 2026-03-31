19d8458252d7c187e8600ae9096f8c28e50a8dc5
poliak2016 <poliak2016@outlook.com>
2026-04-01 00:27:00 +0200

    feat: refine layout and styling of HomeHero component for improved responsiveness and visual consistency

--- src/components/hero/HomeHero.tsx
+++ src/components/hero/HomeHero.tsx
@@ -4,7 +4,7 @@ import { CldImage } from "next-cloudinary";
 
 export default function HomeHero() {
   return (
-    <section className="relative w-full overflow-hidden bg-black aspect-[3/5] sm:aspect-[16/11] lg:aspect-[16/9] min-h-[70svh] max-h-screen">
+    <section className="relative w-full overflow-hidden bg-black min-h-screen sm:aspect-[16/11] lg:aspect-[16/9] px-6 sm:px-10 md:px-20 lg:px-24">
 
       {/* IMAGE */}
       <CldImage
@@ -13,34 +13,28 @@ export default function HomeHero() {
         fill
         sizes="100vw"
         priority
-        className="object-cover object-[16%_center] sm:object-[30%_center] lg:object-[20%_center]"
+        className="object-cover object-center sm:object-[30%_center] lg:object-[20%_center]"
       />
 
       {/* DARK OVERLAY */}
       <div className="absolute inset-0 bg-black/40" />
 
       {/* CONTENT CONTAINER */}
-      <div className="relative z-10 flex h-full w-full items-center justify-end px-6 sm:px-10 md:px-20">
+      <div className="absolute inset-0 z-10 flex items-center justify-center">
 
         {/* TEXT BLOCK */}
-        <div className="max-w-[900px] text-right text-white pr-[5%] md:pr-0">
+        <div className="max-w-[900px] mx-auto text-right text-white md:pr-0">
 
-          
-
-          <h1 className="
-            font-light
-            leading-[0.9]
-            text-[clamp(3rem,8vw,5rem)] sm:text-[clamp(2rem,5vw,5rem)]
-            uppercase
-          ">
+          <h1 className="font-light leading-[0.9] text-5xl sm:text-6xl lg:text-7xl uppercase">
             Konrad <br />
             Kalinowski
           </h1>
-          <p className="tracking-[0.4em] text-sm md:text-xs mb-4 text-neutral-500">
+
+          <p className="tracking-[0.4em] text-white/70 text-xs sm:text-sm mb-4">
             Photography
           </p>
 
-          <p className="mt-6 tracking-[0.3em] text-sm md:text-xs text-neutral-300">
+          <p className="mt-6 tracking-[0.3em] text-xs sm:text-sm text-neutral-300">
             FOOD, DRINKS & PEOPLE
           </p>
 
@@ -49,12 +43,9 @@ export default function HomeHero() {
       </div>
 
       {/* SCROLL INDICATOR */}
-      <div
-  aria-hidden="true"
-  className=" scroll-indicator absolute bottom-[3vh] left-1/2 -translate-x-1/2 text-white text-[clamp(0.6rem,1vw,0.875rem)] tracking-[0.3em]"
->
-  SCROLL
-</div>
+      <div aria-hidden="true" className="scroll-indicator absolute bottom-6 left-1/2 -translate-x-1/2 text-white text-xs sm:text-sm tracking-[0.3em]">
+        SCROLL
+      </div>
 
     </section>
   );
